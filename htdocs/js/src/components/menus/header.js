8bed6fecf8c024dfe69fbc505f6cbdbac5fdcde0
Thibault Kummer <bob@coldsource.net>
2022-04-06 19:30:57 +0200

    Icon change

--- htdocs/js/src/components/menus/header.js
+++ htdocs/js/src/components/menus/header.js
@@ -80,7 +80,7 @@ export class HeaderMenu extends evQueueComponent {
 				label: 'External Logs',
 				icon: 'fa-podcast',
 				submenu: [
-					{ label: 'Channel groups', icon: 'fa-road', url: 'elogs-channel-groups' },
+					{ label: 'Channel groups', icon: 'fa-clone', url: 'elogs-channel-groups' },
 					{ label: 'Channels', icon: 'fa-road', url: 'elogs-channels' },
 					{ label: 'Logs', icon: 'fa-file-text-o', url: 'elogs-search' },
 					{ label: 'Statistics', icon: 'fa-area-chart', url: 'elogs-stats' }
