ec0b22b595f72395de37f82add5332bf7c6b7fb2
Thibault Kummer <bob@coldsource.net>
2022-06-08 18:48:49 +0200

    UTF8 compatible base64 encoding / decoding

---
+++ htdocs/js/src/utils/base64.js
@@ -0,0 +1,35 @@
+ /*
+  * This file is part of evQueue
+  *
+  * evQueue is free software: you can redistribute it and/or modify
+  * it under the terms of the GNU General Public License as published by
+  * the Free Software Foundation, either version 3 of the License, or
+  * (at your option) any later version.
+  *
+  * evQueue is distributed in the hope that it will be useful,
+  * but WITHOUT ANY WARRANTY; without even the implied warranty of
+  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+  * GNU General Public License for more details.
+  *
+  * You should have received a copy of the GNU General Public License
+  * along with evQueue. If not, see <http://www.gnu.org/licenses/>.
+  *
+  * Author: Thibault Kummer
+  */
+ 
+'use strict';
+
+export class Base64Utils {
+	static encodeUTF8(str) {
+		return btoa(encodeURIComponent(str).replace(/%([0-9A-F]{2})/g,
+			function toSolidBytes(match, p1) {
+				return String.fromCharCode('0x' + p1);
+		}));
+	}
+	
+	static decodeUTF8(str) {
+		return decodeURIComponent(atob(str).split('').map(function (c) {
+			return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+		}).join(''));
+	}
+}
